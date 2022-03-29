62e4424da7bafcf69105d1e6525c376af56267c1
Kipngetich33 <khalifngeno@gmail.com>
2022-03-29 22:39:59 +0300

    day 3 days of blockchain

---
+++ index.mjs
@@ -0,0 +1,40 @@
+import { loadStdlib } from '@reach-sh/stdlib';
+import * as backend from './build/index.main.mjs';
+
+const stdlib = loadStdlib(process.env);
+
+// console.log("this is the stdlib",stdlib.newTestAccount);
+
+(async () => {
+    const startingBalance = stdlib.parseCurrency(10);
+
+    const accAlice = await stdlib.newTestAccount(startingBalance);
+    const accBob = await stdlib.newTestAccount(startingBalance);
+
+    const ctcAlice = accAlice.contract(backend)
+    const ctcBob = accBob.contract(backend, ctcAlice.getInfo())
+
+    const HAND = ['Rock','Paper','Scissors']
+    const OUTCOME = ['Bob wins','Draw','Alice wins']
+
+    const Player = (Who) => ({
+        getHand: () =>{
+            const hand = Math.floor((Math.random() * 3))
+            console.log(`${Who} played ${HAND[hand]}`);
+            return hand;
+        },
+        seeOutcome: (outcome) =>{
+            console.log(`${Who} saw outcome ${OUTCOME[outcome]}`)
+        }
+    })
+
+    await Promise.all([
+        backend.Alice(ctcAlice, {
+            ...Player('Alice'),
+        }),
+        backend.Bob(ctcBob, {
+            ...Player('Bob'),
+        })
+    ])
+})();
+
